5dbb3bc29c5e3d94093a633e25bfd314b6c6433b
Ricky <143243737+DrRicx@users.noreply.github.com>
2024-04-30 02:38:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillMembers() {
+  var members = [
+    {name: 'name1', skill: 'skill1'},
+    {name: 'name2', skill: 'skill2'},
+    {name: 'name3', skill: 'skill1'},
+    {name: 'name4', skill: 'skill3'},
+    {name: 'name5', skill: 'skill2'}
+  ];
+  return members;
+}
