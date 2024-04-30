23c3146ff6240f83a6021639ede199a39b60fe71
Ricky <143243737+DrRicx@users.noreply.github.com>
2024-04-30 03:10:56 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,10 +1,9 @@
 function skillMember() {
-    // this is a private function that can't be accessed from outside
-    function getSkill() {
-        return "JavaScript";
-    }
-    // this is a public function
-    this.getSkill = function() {
-        return getSkill();
+    var member = {
+        name: 'member',
+        age: 25,
+        job: 'developer',
+        skill: 'coding'
     };
+    return member;
 }
