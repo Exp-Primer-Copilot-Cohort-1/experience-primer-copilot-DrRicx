a52ab4bf5bf3bdaf4f705b955693628e90fdd47f
Ricky <143243737+DrRicx@users.noreply.github.com>
2024-04-30 02:47:32 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,16 +1,15 @@
-function skillMembers() {
-    var members = [
-        {name: 'John', age: 27},
-        {name: 'Jane', age: 22},
-        {name: 'Paul', age: 31},
-        {name: 'Steve', age: 23},
-        {name: 'Maria', age: 29},
-        {name: 'Tom', age: 24}
-    ];
-    return {
-        members: members,
-        getMember: function (index) {
-            return members[index];
+function skillMember() {
+    var member = {
+        name: 'John',
+        age: 25,
+        skills: ['HTML', 'CSS', 'JS'],
+        address: {
+            city: 'New York',
+            country: 'USA'
+        },
+        getSkill: function () {
+            return this.skills;
         }
     };
+    return member;
 }
