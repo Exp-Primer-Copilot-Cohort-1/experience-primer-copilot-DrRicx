3e807037027d5bdf2eef6d8bcba0dac558aef1ca
Ricky <143243737+DrRicx@users.noreply.github.com>
2024-04-30 03:41:43 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,73 +1,33 @@
-//create web server
-var http = require('http');
-var url = require('url');
+//Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 var fs = require('fs');
 var path = require('path');
-var comments = require('./comments');
-var mimeTypes = {
-    "html": "text/html",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpg",
-    "png": "image/png",
-    "js": "text/javascript",
-    "css": "text/css"
-};
+var commentsPath = path.join(__dirname, 'comments.json');
 
-//create server
-http.createServer(function (req, res) {
-    var uri = url.parse(req.url).pathname;
-    var fileName = path.join(process.cwd(), unescape(uri));
-    console.log('Loading ' + uri);
-    var stats;
+//Middleware
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
-    try {
-        stats = fs.lstatSync(fileName);
-    } catch (e) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('404 Not Found\n');
-        res.end();
-        return;
+//GET /comments
+app.get('/comments', function(req, res) {
+  fs.readFile(commentsPath, 'utf8', function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
     }
-
-    if (stats.isFile()) {
-        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
-        res.writeHead(200, {'Content-Type': mimeType});
-
-        var fileStream = fs.createReadStream(fileName);
-        fileStream.pipe(res);
-    } else if (stats.isDirectory()) {
-        res.writeHead(302, {
-            'Location': 'index.html'
-        });
-        res.end();
-    } else {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.write('500 Internal server error\n');
-        res.end();
-    }
-}).listen(1337);
-
-//create web socket
-var io = require('socket.io').listen(8000);
-io.sockets.on('connection', function (socket) {
-    //listen for new comment
-    socket.on('newComment', function (data) {
-        comments.addComment(data);
-        io.sockets.emit('commentsChanged', comments.getComments());
-    });
-
-    //listen for delete comment
-    socket.on('deleteComment', function (data) {
-        comments.deleteComment(data);
-        io.sockets.emit('commentsChanged', comments.getComments());
-    });
-
-    //listen for change comment
-    socket.on('changeComment', function (data) {
-        comments.changeComment(data);
-        io.sockets.emit('commentsChanged', comments.getComments());
-    });
-
-    //send comments to client
-    socket.emit('commentsChanged', comments.getComments());
+    res.json(JSON.parse(data));
+  });
 });
+
+//POST /comments
+app.post('/comments', function(req, res) {
+  fs.readFile(commentsPath, 'utf8', function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    var newComment = {
+      id: Date.now(),
